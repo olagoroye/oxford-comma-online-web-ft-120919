744d305b98bb77478d57cbb1d366495e1efaa6f1
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-11 16:29:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.length == 1 
+  array.join
+  elsif array.length == 2
+      array.join(" and ")
+   else
+  lastStr = array.pop
+       array.join(", ") << ", and #{lastStr}"
+     end
 end
